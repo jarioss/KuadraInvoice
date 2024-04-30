4fce97bdc68e85001b874d051ab2aeae0fff0114
jarioss <armando.rios@udea.edu.co>
2024-04-30 12:22:23 -0500

    first commit

---
+++ src/main/java/com/kuadra/invoice/KuadraInvoiceApplication.java
@@ -0,0 +1,13 @@
+package com.kuadra.invoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuadraInvoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuadraInvoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuadra/invoice/KuadraInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuadra.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuadraInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
